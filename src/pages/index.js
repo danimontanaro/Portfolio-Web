39baf27c7a68d6e75f1249095069364becc51d3a
Daniela Montanaro <dani.montanaro21@gmail.com>
2020-04-08 16:45:51 -0300

    remove twitter and mail

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Container, Row, Col } from 'react-awesome-styled-grid'
-import { FaGithub, FaLinkedin, FaEnvelope, FaTwitter } from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import siteConfig from '../../data/siteConfig'
 
 import Layout from '../components/layout'
